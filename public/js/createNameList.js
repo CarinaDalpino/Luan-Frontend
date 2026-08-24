329ebc6fb02f92493d3f9ab862e64b3617f74e1b
Carina Dalpino <carina.g.santos22@gmail.com>
2026-08-24 20:46:20 -0300

    Implementa edicao de itens da lista

--- public/js/createNameList.js
+++ public/js/createNameList.js
@@ -1,22 +1,30 @@
+import editNameList from "./editNameList.js";
+
 export default function createNameList(name) {
 
     const liElement = document.createElement("li");
     liElement.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
 
-    liElement.append(document.createTextNode(name));
+    const nameElement = document.createElement("span");
+    nameElement.classList.add("list-name");
+    nameElement.append(document.createTextNode(name));
+    liElement.append(nameElement);
 
-    const buttonDeleteElement = document.createElement("button");
-    buttonDeleteElement.classList.add("btn", "btn-danger", "btn-sm");
-    buttonDeleteElement.innerText = "Excluir";
-    buttonDeleteElement.addEventListener("click", (event) => {
-        event.preventDefault();
+    const createDeleteButton = () => {
+        const buttonDeleteElement = document.createElement("button");
+        buttonDeleteElement.classList.add("btn", "btn-danger", "btn-sm");
+        buttonDeleteElement.innerText = "Excluir";
+        buttonDeleteElement.addEventListener("click", (event) => {
+            event.preventDefault();
 
-        console.log("target:", event.target);
-        console.log("currentTarget:", event.currentTarget);
+            event.currentTarget.parentElement.remove();
+        });
 
-        event.currentTarget.parentElement.remove();
-    });
-    liElement.append(buttonDeleteElement);
+        return buttonDeleteElement;
+    };
+
+    liElement.append(createDeleteButton());
+    editNameList(liElement, createDeleteButton);
     return liElement;
 
 }
