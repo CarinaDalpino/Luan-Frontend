6ff1354501b4d4f96005e061f3b0f5fac9f3497d
Carina Dalpino <carina.g.santos22@gmail.com>
2026-08-24 20:50:17 -0300

    Restaura pastas das aulas 01 e 02

---
+++ aula-02/unifaat-frontend-bimestre-01/public/js/createNameList.js
@@ -0,0 +1,32 @@
+export default function createNameList(name) {
+
+    const liElement = document.createElement("li");
+    liElement.classList.add(
+        "list-group-item",
+        "d-flex",
+        "justify-content-between",
+        "align-items-center"
+    );
+
+    const nameElement = document.createElement("span");
+    nameElement.innerText = name;
+
+    liElement.append(nameElement);
+
+    const buttonDeleteElement = document.createElement("button");
+    buttonDeleteElement.classList.add("btn", "btn-danger", "btn-sm");
+    buttonDeleteElement.innerText = "Excluir";
+
+    buttonDeleteElement.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        console.log("target:", event.target);
+        console.log("currentTarget:", event.currentTarget);
+
+        event.currentTarget.parentElement.remove();
+    });
+
+    liElement.append(buttonDeleteElement);
+
+    return liElement;
+}
